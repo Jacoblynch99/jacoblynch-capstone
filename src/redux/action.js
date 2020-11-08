e340acf6df7ecaca1140e98ba9b13218c75d2c40
Jacoblynch99 <jacoblynch99@gmail.com>
2020-11-08 15:14:01 -0600

    API INTEGRATED

--- src/redux/action.js
+++ src/redux/action.js
@@ -2,13 +2,18 @@ import axios from 'axios'
 
 export const getBusinesses = () => {
     return function (dispatch) {
-        axios.get('/users').then((res) => {
-            const action = {
-                type: 'GET_BUSINESSES',
-                value: res.data,
-            }
-            dispatch(action)
-        })
+        axios
+            .get('/users')
+            .then((res) => {
+                const action = {
+                    type: 'GET_BUSINESSES',
+                    value: res.data,
+                }
+                dispatch(action)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 }
 
@@ -26,24 +31,34 @@ export const getBusinesses = () => {
 
 export const createTicket = (postBody) => {
     return function (dispatch) {
-        axios.post('/users', { postBody }).then((res) => {
-            const action = {
-                type: 'POST_TICKET',
-                value: res.data,
-            }
-            dispatch(action)
-        })
+        axios
+            .post('/users', { postBody })
+            .then((res) => {
+                const action = {
+                    type: 'POST_TICKET',
+                    value: res.data,
+                }
+                dispatch(action)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 }
 
 export const getQuotes = () => {
     return function (dispatch) {
-        axios.get('https://type.fit/api/quotes').then((res) => {
-            const action = {
-                type: 'GET_QUOTES',
-                value: res.data,
-            }
-            dispatch(action)
-        })
+        axios
+            .get('https://type.fit/api/quotes')
+            .then((res) => {
+                const action = {
+                    type: 'GET_QUOTES',
+                    value: res.data,
+                }
+                dispatch(action)
+            })
+            .catch((err) => {
+                console.log(err)
+            })
     }
 }
