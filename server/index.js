a1fe65ca6c69e45db9ae8f6a57b5abd8721449cf
Jacoblynch99 <jacoblynch99@gmail.com>
2020-11-08 16:02:20 -0600

    API INTEGRATED

--- server/index.js
+++ server/index.js
@@ -3,17 +3,39 @@ const bodyParser = require('body-parser')
 const usersRouter = require('./routers/users')
 const authRouter = require('./routers/auth')
 const path = require('path')
-
+const app = express()
 const cors = require('cors')
 
 require('dotenv').config()
 
-const app = express()
 const port = process.env.PORT || 4001
 
-// app.use(express.static(path.join(__dirname, 'build')))
-
 app.use(cors())
+
+//
+
+//
+
+//
+
+//
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'client/build')))
+    app.get('*', (req, res) => {
+        res.sendfile(path.join((__dirname = 'client/build/index.html')))
+    })
+}
+
+app.use(express.static(path.join(__dirname, 'client/build')))
+
+//
+
+//
+
+//
+
+//
 
 app.use(bodyParser.json())
 app.use('/users', usersRouter)
