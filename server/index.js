dafc7265520cf5b09190257bfcbe26a1978e9cb5
Jacoblynch99 <jacoblynch99@gmail.com>
2020-11-08 16:31:55 -0600

    API INTEGRATED

--- server/index.js
+++ server/index.js
@@ -12,29 +12,6 @@ const port = process.env.PORT || 4001
 
 app.use(cors())
 
-//
-
-//
-
-//
-
-//
-
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')))
-    app.get('*', (req, res) => {
-        res.sendfile(path.join((__dirname = 'client/build/index.html')))
-    })
-}
-
-//
-
-//
-
-//
-
-//
-
 app.use(bodyParser.json())
 app.use('/users', usersRouter)
 app.use('/auth', authRouter)
